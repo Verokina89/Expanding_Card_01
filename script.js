98c3f4225904da89b8a22a9360bff0ca059b20f5
Verónica I Pérez S. <v.isabel1989@gmail.com>
2024-08-07 18:30:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.imgCard')
+    
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            removeActiveClass();
+            card.classList.add('active');
+        });
+    
+    });
+    
+    function removeActiveClass() {
+        cards.forEach(card => {
+            card.classList.remove('active')
+        })
+    }
+})
+
+
+/*-------Condicionales --------*/
+// const cards = document.querySelectorAll('.imgCard');
+
+// // Recorre cada elemento en 'cards' y añade el evento 'click'
+// cards.forEach(card => {
+//     card.addEventListener('click', () => {
+//         //se llama a la funcion que pasa como argumento la imagen clicada
+//         toggleActive(card);
+//     });
+// });
+
+// //se define la funcion que activar/desactivar las imagenes
+// const toggleActive = (eventClick) => {
+//     // Si la img esta clicada 'active', queda igual
+//     if (eventClick.classList.contains('active')) {
+//         return;
+//     } else {//si la img no esta 'active'
+//         //recorre las 'cards' y elimina la clase 'active'.
+//         cards.forEach(card => {
+//             card.classList.remove('active');
+//         });
+//         // Añade la clase 'active' a la img clicada.
+//         eventClick.classList.add('active');
+//     }
+// };
+
+
+
